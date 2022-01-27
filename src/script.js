0848e9619be86f8a16a89ae5b628869be09f7a1f
Chris Johnson <49479599+workeffortwaste@users.noreply.github.com>
2022-01-27 21:16:42 +0000

    Add additional menu items

--- src/script.js
+++ src/script.js
@@ -4,80 +4,8 @@
   /* Libaries */
   const FileSaver = require('file-saver')
 
-  /* Watch the lighthouse window object to capture changes */
-  const update = async (lighthouseData) => {
-    if (window.__LIGHTHOUSE_JSON__?.configSettings?.formFactor === 'mobile') {
-      window.pageSpeedSaverMobile = lighthouseData
-      injectHTML('mobile')
-    }
-    if (window.__LIGHTHOUSE_JSON__?.configSettings?.formFactor === 'desktop') {
-      window.pageSpeedSaverDesktop = lighthouseData
-      injectHTML('desktop')
-    }
-  }
-
-  Object.defineProperties(window, {
-    ___LIGHTHOUSE_JSON__: {
-      value: {},
-      writable: true
-    },
-    __LIGHTHOUSE_JSON__: {
-      get: function () {
-        return this.___LIGHTHOUSE_JSON__
-      },
-      set: function (val) {
-        this.___LIGHTHOUSE_JSON__ = val
-        update(this.___LIGHTHOUSE_JSON__)
-      }
-    }
-  })
-
-  /* Button injection observer */
-  const injectHTML = (device) => {
-    const num = device === 'mobile' ? 0 : 1
-    const target = [...document.querySelectorAll('div[role="tabpanel"] c-wiz')].slice(-2)[num]
-    let toggle = false
-
-    const callback = async (mutationsList, observer) => {
-      if (toggle) return; toggle = true
-      console.log(`Injecting PageSpeed Saver HTML (${device})`)
-      await sleep(1000);
-      // Insert button
-      [...document.querySelectorAll(`*[aria-labelledby="${device}_tab"] button#url_tab`)].pop().insertAdjacentHTML('afterend', escapeHTMLPolicy.createHTML(html))
-
-      // Hook button to JSON
-      const button = [...document.querySelectorAll(`*[aria-labelledby="${device}_tab"] .pageSpeed_button`)].pop()
-      if (device === 'desktop') {
-        button.addEventListener('click', e => {
-          console.log('bep')
-          FileSaver.saveAs(new Blob([JSON.stringify(window.pageSpeedSaverDesktop)]), 'pagespeed-saver-json-mobile.json')
-        })
-      } else {
-        button.addEventListener('click', e => {
-          console.log('bep')
-          FileSaver.saveAs(new Blob([JSON.stringify(window.pageSpeedSaverMobile)]), 'pagespeed-saver-json-mobile.json')
-        })
-      }
-
-      observer.disconnect()
-    }
-
-    const observer = new MutationObserver(callback)
-    observer.observe(target, { attributes: true, childList: true, subtree: true })
-  }
-
-  /* ES6 sleep */
-  const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms))
-  }
-
-  /* HTML injection helper */
-  const escapeHTMLPolicy = trustedTypes.createPolicy('forceInner', {
-    createHTML: (content) => content
-  })
-
   /* The HTML payload */
-  const html = `
+  const htmlButtons = `
     <button
       class="pageSpeed_button"
       role="tab"
@@ -141,4 +69,99 @@
         height: 100%;
       }
     </style>`
+
+  const htmlMenu = `
+    <style>
+      header a {
+        margin-right: auto;
+      }
+      header {
+        justify-content: flex-end;
+      }
+    </style>
+  `
+
+  /* Watch the lighthouse window object to capture changes */
+  const update = async (lighthouseData) => {
+    if (window.__LIGHTHOUSE_JSON__?.configSettings?.formFactor === 'mobile') {
+      window.pageSpeedSaverMobile = lighthouseData
+      injectHTML('mobile')
+    }
+    if (window.__LIGHTHOUSE_JSON__?.configSettings?.formFactor === 'desktop') {
+      window.pageSpeedSaverDesktop = lighthouseData
+      injectHTML('desktop')
+    }
+  }
+
+  Object.defineProperties(window, {
+    ___LIGHTHOUSE_JSON__: {
+      value: {},
+      writable: true
+    },
+    __LIGHTHOUSE_JSON__: {
+      get: function () {
+        return this.___LIGHTHOUSE_JSON__
+      },
+      set: function (val) {
+        this.___LIGHTHOUSE_JSON__ = val
+        update(this.___LIGHTHOUSE_JSON__)
+      }
+    }
+  })
+
+  /* Button injection observer */
+  const injectHTML = (device) => {
+    const num = device === 'mobile' ? 0 : 1
+    const target = [...document.querySelectorAll('div[role="tabpanel"] c-wiz')].slice(-2)[num]
+    let toggle = false
+
+    const callback = async (mutationsList, observer) => {
+      if (toggle) return; toggle = true
+      console.log(`Injecting PageSpeed Saver HTML (${device})`)
+      await sleep(1000);
+      // Insert button
+      [...document.querySelectorAll(`*[aria-labelledby="${device}_tab"] button#url_tab`)].pop().insertAdjacentHTML('afterend', escapeHTMLPolicy.createHTML(htmlButtons))
+
+      // Hook button to JSON
+      const button = [...document.querySelectorAll(`*[aria-labelledby="${device}_tab"] .pageSpeed_button`)].pop()
+      if (device === 'desktop') {
+        button.addEventListener('click', e => {
+          console.log('bep')
+          FileSaver.saveAs(new Blob([JSON.stringify(window.pageSpeedSaverDesktop)]), 'pagespeed-saver-json-mobile.json')
+        })
+      } else {
+        button.addEventListener('click', e => {
+          console.log('bep')
+          FileSaver.saveAs(new Blob([JSON.stringify(window.pageSpeedSaverMobile)]), 'pagespeed-saver-json-mobile.json')
+        })
+      }
+
+      observer.disconnect()
+    }
+
+    const observer = new MutationObserver(callback)
+    observer.observe(target, { attributes: true, childList: true, subtree: true })
+  }
+
+  /* ES6 sleep */
+  const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+
+  /* HTML injection helper */
+  const escapeHTMLPolicy = trustedTypes.createPolicy('forceInner', {
+    createHTML: (content) => content
+  })
+
+  /* Add additional menu buttons */
+  const link = document.querySelector('header > div')
+  const linkOpen = link.cloneNode(true)
+  const linkDiff = link.cloneNode(true)
+  linkOpen.querySelector('a').setAttribute('href', 'https://googlechrome.github.io/lighthouse/viewer/')
+  linkOpen.querySelector('span').textContent = 'Open Report'
+  linkDiff.querySelector('a').setAttribute('href', 'https://googlechrome.github.io/lighthouse-ci/viewer/')
+  linkDiff.querySelector('span').textContent = 'Compare Reports'
+  link.after(linkOpen)
+  link.after(linkDiff)
+  document.body.insertAdjacentHTML('beforeend', escapeHTMLPolicy.createHTML(htmlMenu))
 })()
