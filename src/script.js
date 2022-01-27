5685ff00310b279b61085b62f78301e3c6346aa2
Chris Johnson <49479599+workeffortwaste@users.noreply.github.com>
2022-01-27 16:33:01 +0000

    Support the new version of PageSpeed Insights

--- src/script.js
+++ src/script.js
@@ -1,92 +1,144 @@
-// PageSpeed Saver 1.2.3
+// PageSpeed Saver 2.0.0
 // @defaced
 (() => {
-  const pageSpeedSaverReset = () => {
-    window.pageSpeedSaverMobile = null
-    window.pageSpeedSaverDesktop = null
-    const saver = document.getElementById('pageSpeedSaver')
-    const status = document.getElementById('pageSpeedSaverStatus')
-    if (status) { status.remove() }
-    if (saver) { saver.remove(); document.getElementById('pageSpeedStyles').remove() }
-  }
+  /* Libaries */
+  const FileSaver = require('file-saver')
 
-  const { fetch: origFetch } = window
-  window.fetch = async (...args) => {
-    if (document.getElementById('pageSpeedSaver') || document.getElementById('pageSpeedSaverStatus')) { pageSpeedSaverReset() }
-    const response = await origFetch(...args)
-    response
-      .clone()
-      .json()
-      .then(body => pageSpeedSaver(body))
-      .catch(err => console.error(err))
-
-    return response
-  }
-
-  if (document.getElementsByClassName('progress-value')[0].innerText !== '0') {
-    const html = '<p style="text-align: center;color: #f7f7f7;" id="pageSpeedSaverStatus">PageSpeed Saver - Report already in progress, cannot capture data.</p>'
-    document.getElementsByClassName('url-and-analyze')[0].insertAdjacentHTML('afterend', html)
-  }
-
-  const pageSpeedSaver = (content) => {
-    if (content.kind === 'pagespeedonline#result') {
-      if (content.lighthouseResult.configSettings.emulatedFormFactor === 'desktop') {
-        window.pageSpeedSaverDesktop = content
-      } else {
-        window.pageSpeedSaverMobile = content
-      }
-      /* When both the window variables are set load the HTML */
-      if (window.pageSpeedSaverDesktop && window.pageSpeedSaverMobile) {
-        pageSpeedSaveHTML()
-      }
+  /* Watch the lighthouse window object to capture changes */
+  const update = async (lighthouseData) => {
+    if (window.__LIGHTHOUSE_JSON__?.configSettings?.formFactor === 'mobile') {
+      window.pageSpeedSaverMobile = lighthouseData
+      injectHTML('mobile')
+    }
+    if (window.__LIGHTHOUSE_JSON__?.configSettings?.formFactor === 'desktop') {
+      window.pageSpeedSaverDesktop = lighthouseData
+      injectHTML('desktop')
     }
   }
 
-  const FileSaver = require('file-saver')
+  Object.defineProperties(window, {
+    ___LIGHTHOUSE_JSON__: {
+      value: {},
+      writable: true
+    },
+    __LIGHTHOUSE_JSON__: {
+      get: function () {
+        return this.___LIGHTHOUSE_JSON__
+      },
+      set: function (val) {
+        this.___LIGHTHOUSE_JSON__ = val
+        update(this.___LIGHTHOUSE_JSON__)
+      }
+    }
+  })
 
-  const pageSpeedSaveHTML = () => {
-    const html = `
-        <style id="pageSpeedStyles">
-        #pageSpeedSaver {
-            position: absolute;
-            z-index: 999999999;
-            right: 20px;
-            padding: 0;
-            height:100%;
-            padding-bottom: -30px;
-            box-shadow: none;
-            background: transparent;
-            transform: translateY(4px);
-            pointer-events: none;
-        }
-        #pageSpeedSaver button{
-            cursor:pointer;
-            position:sticky;
-            top: 51px;
-            pointer-events: all;
-        }
-        #pageSpeedSaverDesktop {
-            margin-left: 4px;
-        }
-        @media only screen and (max-width: 720px) {
-            #pageSpeedSaver {
-                display:none;
-            }
-        }
-        </style>
-        <div class="main-action" id="pageSpeedSaver">
-            <button id="pageSpeedSaverMobile" class="button button-red analyze jfk-button main-submit jfk-button-standard">Save Mobile JSON</button>
-            <button id="pageSpeedSaverDesktop" class="button button-red analyze jfk-button main-submit jfk-button-standard">Save Desktop JSON</button>
-        </div>`
+  /* Button injection observer */
+  const injectHTML = (device) => {
+    const num = device === 'mobile' ? 0 : 1
+    const target = [...document.querySelectorAll('div[role="tabpanel"] c-wiz')].slice(-2)[num]
+    let toggle = false
 
-    document.getElementsByClassName('pagespeed-results')[0].insertAdjacentHTML('beforebegin', html)
+    const callback = async (mutationsList, observer) => {
+      if (toggle) return; toggle = true
+      console.log(`Injecting PageSpeed Saver HTML (${device})`)
+      await sleep(1000);
+      // Insert button
+      [...document.querySelectorAll(`*[aria-labelledby="${device}_tab"] button#url_tab`)].pop().insertAdjacentHTML('afterend', escapeHTMLPolicy.createHTML(html))
 
-    document.getElementById('pageSpeedSaverMobile').addEventListener('click', e => {
-      FileSaver.saveAs(new Blob([JSON.stringify(window.pageSpeedSaverMobile.lighthouseResult)]), 'pagespeed-saver-json-mobile.json')
-    })
+      // Hook button to JSON
+      const button = [...document.querySelectorAll(`*[aria-labelledby="${device}_tab"] .pageSpeed_button`)].pop()
+      if (device === 'desktop') {
+        button.addEventListener('click', e => {
+          console.log('bep')
+          FileSaver.saveAs(new Blob([JSON.stringify(window.pageSpeedSaverDesktop)]), 'pagespeed-saver-json-mobile.json')
+        })
+      } else {
+        button.addEventListener('click', e => {
+          console.log('bep')
+          FileSaver.saveAs(new Blob([JSON.stringify(window.pageSpeedSaverMobile)]), 'pagespeed-saver-json-mobile.json')
+        })
+      }
 
-    document.getElementById('pageSpeedSaverDesktop').addEventListener('click', e => {
-      FileSaver.saveAs(new Blob([JSON.stringify(window.pageSpeedSaverDesktop.lighthouseResult)]), 'pagespeed-saver-json-desktop.json')
-    })
+      observer.disconnect()
+    }
+
+    const observer = new MutationObserver(callback)
+    observer.observe(target, { attributes: true, childList: true, subtree: true })
   }
+
+  /* ES6 sleep */
+  const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+
+  /* HTML injection helper */
+  const escapeHTMLPolicy = trustedTypes.createPolicy('forceInner', {
+    createHTML: (content) => content
+  })
+
+  /* The HTML payload */
+  const html = `
+    <button
+      class="pageSpeed_button"
+      role="tab"
+      aria-selected="false"
+      tabindex="-1"
+      data-skip-focus-on-activate="false"
+    >
+      <span class="pageSpeed_button__container">
+        <span class="pageSpeed_button__container__text">Download Report</span>
+      </span>
+      <span class="pageSpeed_button__container__background"></span>
+    </button>
+
+    <style>
+      .pageSpeed_button{
+        font-family: "Google Sans",Roboto,Arial,sans-serif;
+        line-height: 1.25rem;
+        font-size: .875rem;
+        letter-spacing: .0178571429em;
+        font-weight: 500;
+        text-transform: none;
+        min-width: auto;
+        background: none;
+        min-width: 90px;
+        display: flex;
+        flex: 1 0 auto;
+        justify-content: center;
+        box-sizing: border-box;
+        margin: 0;
+        border: none;
+        outline: none;
+        text-align: center;
+        white-space: nowrap;
+        cursor: pointer;
+        -webkit-appearance: none;
+        z-index: 1;
+        position:relative;
+      }
+
+      .pageSpeed_button__container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: inherit;
+        pointer-events: none;
+        place-self: center;
+      }
+
+      .pageSpeed_button__container__text {
+        color: #5f6368;
+        transition: 150ms color linear;
+        display: inline-block;
+        line-height: 1;
+        z-index: 2;
+      }
+
+      .pageSpeed_button:hover .pageSpeed_button__container__background{
+        background-color: #f8f9fa;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+      }
+    </style>`
 })()
